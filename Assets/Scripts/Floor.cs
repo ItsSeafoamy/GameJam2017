6b4e098b6bae4561de0d44cee41b59f54e78cb5f
Laxynd <laxynd@siblaze.com>
2017-01-20 21:51:22 +0000

    *waves*

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class Floor : MonoBehaviour {
 
 	public AnimationCurve wave;
-	public int size;
+	public float speed;
 
 	private void FixedUpdate() {
-		float t = Time.time % 1;
+		float t = Time.time * speed;
 
 		TerrainData data = GetComponent<Terrain>().terrainData;
 
-		float[,] heights = new float[(int) data.size.z, (int) data.size.x];
+		float[,] heights = new float[(int) data.heightmapResolution, (int) data.heightmapResolution];
 
-		for (int x = 0; x < data.size.x; x++) {
-			for (int y = 0; y < data.size.z; y++) {
-				float radius = Mathf.Atan2(data.size.z/2 - y, data.size.x/2 - x);
+		for (int x = 0; x < data.heightmapResolution; x++) {
+			for (int y = 0; y < data.heightmapResolution; y++) {
+				float radius = Mathf.Atan2(data.heightmapResolution/2 - y, data.heightmapResolution/2 - x);
 
-				heights[y, x] = (wave.Evaluate(t + (radius/32f))) / 50f;
+				float dist = Vector3.Distance(new Vector3(x, y), new Vector3(data.heightmapResolution/2, data.heightmapResolution/2));
+
+				heights[y, x] = (wave.Evaluate((t + (dist/64f))%1)) / 50f;
 			}
 		}
 
